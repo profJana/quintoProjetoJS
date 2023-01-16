3634894381225e634241eae1e50722b2d5cb0927
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-01-15 22:38:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function resultSoma(){
+    var result =
+        Number(document.getElementById("firstNumber").value) +
+        Number(document.getElementById("secondNumber").value);
+        window.alert(result);
+  }
